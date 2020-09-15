a8f351cef1e216b957aa24e5399a68ba958da047
Anas Syed <a.syed91@gmail.com>
2020-09-15 23:35:46 +0200

    make snake to move

--- week1/snake/snake.js
+++ week1/snake/snake.js
@@ -18,11 +18,12 @@ let snake = [
 let food = {x: 15, y: 15};
 
 function snakeEquals(a, b) { 
-	/* fill here */
+    return a.x === b.x && a.y === b.y
 }
 
 function changeDirection(orientation) {
-    /* fill here */
+    const idx = orientation.indexOf(direction);
+    direction = orientation[idx + 1];
 }
 
 function start() {
@@ -62,10 +63,9 @@ function nextBoard() {
         food.x = Math.floor(Math.random() * 20);   // place new food at random location
         food.y = Math.floor(Math.random() * 20);
     } else {
-        /* fill here */ // no food found => no growth despite new head => remove last element
+        snake.pop()
     }
-
-    /* fill here */; // put head at front of the list
+    snake.unshift(head)
 }
 
 function display(context) {
